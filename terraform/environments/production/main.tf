6ade1f304118608fa11efa781acd074717e7f457
roberto <robpalacios1@hotmail.com>
2026-09-22 16:12:30 -0500

    fix VPC module name

--- terraform/environments/production/main.tf
+++ terraform/environments/production/main.tf
@@ -3,10 +3,10 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = "../modules/vpc"
+  source = "../../modules/vpc"
   
-  vpc_cidr_block     = var.vpc_cidr_block
-  public_subnet_cidr = var.public_subnet_cidr
-  public_subnet_az   = var.public_subnet_az
-  my_ip              = var.my_ip
+  vpc_cidr_block     = module.vpc_cidr_block
+  public_subnet_cidr = module.public_subnet_cidr
+  public_subnet_az   = module.public_subnet_az
+  my_ip              = module.my_ip
 }
