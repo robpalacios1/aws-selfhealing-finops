ccdb280b672177d5063f871e565be6c54d1760a9
roberto <robpalacios1@hotmail.com>
2026-09-22 17:14:54 -0500

    create variables.tf for compute module

---
+++ terraform/modules/compute/variables.tf
@@ -0,0 +1,14 @@
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "security_group_id" {
+  description = "Security group ID"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "Subnet ID"
+  type        = list(string)
+}
