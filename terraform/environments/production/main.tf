64b3caa0c2fc16d144eefb8bf060d6f26b7c1263
roberto <robpalacios1@hotmail.com>
2026-09-22 16:26:26 -0500

    remove code

--- terraform/environments/production/main.tf
+++ terraform/environments/production/main.tf
@@ -4,9 +4,4 @@ provider "aws" {
 
 module "vpc" {
   source = "../../modules/vpc"
-  
-  vpc_cidr_block     = module.vpc_cidr_block
-  public_subnet_cidr = module.public_subnet_cidr
-  public_subnet_az   = module.public_subnet_az
-  my_ip              = module.my_ip
 }
