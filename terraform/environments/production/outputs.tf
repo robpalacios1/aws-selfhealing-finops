efa910958a5bf140e67380defda14544d7d3545c
roberto <robpalacios1@hotmail.com>
2026-09-22 17:31:08 -0500

    Create ASG outputs.tf file

--- terraform/environments/production/outputs.tf
+++ terraform/environments/production/outputs.tf
@@ -1,3 +1,7 @@
+# ====================================================================
+# VPC outputs
+# ====================================================================
+
 output "prod_vpc_id" {
   description = "The ID of the production VPC"
   value       = module.vpc.vpc_id
@@ -11,4 +15,18 @@ output "prod_public_subnet_id" {
 output "prod_security_group_id" {
   description = "The ID of the production security group"
   value       = module.vpc.security_group_id
+}
+
+# ====================================================================
+# Create ASG outputs
+# ====================================================================
+
+output "prod_asg_name" {
+  description = "ASG Name"
+  value       = module.compute.asg_name
+}
+
+output "prod_asg_arn" {
+  description = "ASG ARN"
+  value       = module.compute.asg_arn
 }
