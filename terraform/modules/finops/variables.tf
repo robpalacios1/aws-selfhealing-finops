9bd52f2d0dc159c6f25f82317a1ad82bbc007fa3
roberto <robpalacios1@hotmail.com>
2026-09-23 14:04:43 -0500

    create variables.tf for finops module

---
+++ terraform/modules/finops/variables.tf
@@ -0,0 +1,10 @@
+variable "environment" {
+  description = "environment variable"
+  type        = string
+  default     = "production"
+}
+
+variable "instance_id" {
+  description = "instance id"
+  type        = string
+}
